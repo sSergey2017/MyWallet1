3310ca8d268902a9c0d0001e26381590f6c5f0de
Serhii Sudakov <serhiisudakov@gmail.com>
2024-11-09 12:03:05 +0200

    newService
    added new service

--- MyWallet1/MyWallet1/Program.cs
+++ MyWallet1/MyWallet1/Program.cs
@@ -1,3 +1,6 @@
+using MyWallet1.Interfaces;
+using MyWallet1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<INewService, NewService>();
+
 
 var app = builder.Build();
 
---
+++ MyWallet1/MyWallet1/Services/NewService.cs
@@ -0,0 +1,13 @@
+﻿using MyWallet1.Interfaces;
+
+namespace MyWallet1.Services
+{
+    public class NewService : INewService
+    {
+        public string GetMessage()
+        {
+            return "I am new service";
+        }
+    }
+
+}
